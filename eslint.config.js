532f8ad030ddd3cdd7f1905d485743b1f41e2833
lxpool <lxpool86@gmail.com>
2024-09-28 20:56:02 +0200

    update eslint file

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+// eslint.config.js
+import typeScriptParser from '@typescript-eslint/parser';
+import typeScriptPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['src/**/*.{js,ts}'],
+    languageOptions: {
+      parser: typeScriptParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      globals: {
+        // Definisci le variabili globali necessarie
+        window: 'readonly',
+        document: 'readonly',
+        process: 'readonly',
+        require: 'readonly',
+        // Aggiungi altre variabili globali se necessario
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typeScriptPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      'prettier/prettier': 'error',
+      // Aggiungi altre regole se necessario
+    },
+    // "env" è stato rimosso e sostituito da "globals"
+  },
+];
