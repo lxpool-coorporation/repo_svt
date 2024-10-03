92e6dd3f1845b376eac81671240156b480ef2c96
lxpool <lxpool86@gmail.com>
2024-10-04 00:03:57 +0200

    fix on seeds

---
+++ seeders/20241003214931-utn_profilo_permesso.ts
@@ -0,0 +1,36 @@
+import { Op, QueryInterface, Sequelize } from 'sequelize';
+
+export default {
+  up: async (queryInterface: QueryInterface) => {
+    await queryInterface.bulkInsert('utn_profilo_permesso', [
+      {
+        id_profilo: 1,
+        id_permesso: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id_profilo: 1,
+        id_permesso: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id_profilo: 1,
+        id_permesso: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id_profilo: 2,
+        id_permesso: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ], {});
+  },
+
+  down: async (queryInterface: QueryInterface) => {
+    await queryInterface.bulkDelete('utn_profilo_permesso',{}, {});
+  }
+};
