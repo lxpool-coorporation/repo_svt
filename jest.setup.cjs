0e474368c0bbf8a62df1894a003f30a0c3d980b4
lxpool <lxpool86@gmail.com>
2024-10-04 19:20:28 +0200

    test ci

--- jest.setup.cjs
+++ jest.setup.cjs
@@ -1,6 +1,6 @@
 require('dotenv').config();
 // jest.setup.js
-// Controlla se esiste una variabile d'ambiente `APP_ENV`, altrimenti usa 'test' come fallback
+// Controlla se esiste una variabile d'ambiente `APP_ENV`, altrimenti usa 'test' come fallback 
 process.env.NODE_ENV = process.env.APP_ENV || 'test';
 
 console.log(`NODE_ENV set to: ${process.env.NODE_ENV}`);
